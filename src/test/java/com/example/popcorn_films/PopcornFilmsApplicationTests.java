a6528acaed4c6c50b4e51fdef2e61adb185c9094
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2023-03-17 08:53:46 +0200

    Initial commit

---
+++ src/test/java/com/example/popcorn_films/PopcornFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popcorn_films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
